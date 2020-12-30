1ec9c7ae50cbbca545598ca8dae381d9a978d61b
Impala04 <75026981+Impala04@users.noreply.github.com>
2020-12-30 19:07:43 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,25 @@
+class Dustbin{
+    constructor(x,y){
+   var options= {
+       restitution:0.8,
+       isStatic:true
+   }
+    this.width= 100
+    this.height=100
+    this.body= Bodies.rectangle(x,y,this.width,this.height,options)
+    this.image=loadImage("dustbingreen.png")
+    World.add(world,this.body);
+        } 
+    display(){
+        var pos = this.body.position;
+        var angle= this.body.position;
+        push ()
+        translate(pos.x,pos.y)
+        rotate(angle)
+       // rectMode(CENTER)
+      // fill("red")
+       // rect(0,0,this.width,this.height)
+       image(this.image,0,0,this.width,this.height)
+        pop ()
+    }}
+
